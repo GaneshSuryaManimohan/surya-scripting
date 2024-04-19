daa966167b3a92cb326cc36df6060d698e98ec2c
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-19 20:47:53 +0530

    modified commands.sh

--- commands.sh
+++ commands.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
 # These are the top commands in linux 
-df -h; free -h; whoami; 
+echo "The current disk utilization on the server $HOSTNAME is: "
+df -h; 
+
+echo "*********************************************************"
+
+echo "The current memory utilization or free space on server $HOSTNAME is: "
+free -h; 
+
+echo "*********************************************************"
+
+echo "The current user on server $HOSTNAME is: "
+whoami; 
