9013a86db03b74eda3cbc7e7313f45422b84ae91
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 22:44:14 +0530

    conditions script created

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,8 @@
 
 NUMBER=$1
 
-if{($NUMBER>10)
-    print ("The given number $NUMBER is greater than 10")
-}
-else
-    print ("The given number $NUMBER is less than 10")
+if [$NUMBER -gt 10]; then
+    echo "The given number $NUMBER is greater than 10"
+elif
+    print ("The given number $NUMBER is less than 10 ")
 fi
