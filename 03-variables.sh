2912cb96a2f7dcede4e5626b721db797c6af7a4f
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 18:25:00 +0530

    03-variables.sh created

--- 03-variables.sh
+++ 03-variables.sh
@@ -5,8 +5,8 @@ echo "Please enter the username:: "
 read USERNAME
 useradd $USERNAME
 
+passwd $USERNAME
 echo "Please enter the password:: "
 read -s PASSWORD
-passwd $USERNAME $PASSWORD
 
 echo "user $USERNAME is added"
