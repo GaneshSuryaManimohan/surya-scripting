c9c018438e6ffea54d7e8cdeee81566cc785cd29
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 18:06:56 +0530

    02-variables.sh created

---
+++ 02-variables.sh
@@ -0,0 +1,11 @@
+P1=$1
+P2=$2
+P3=$3
+
+# we defined the variables, to call the variables use $variable as shown below
+
+echo "$P1:: Hi $P2, Can we get to see you play in next year's IPL?"
+echo "$P2:: What do you think $P1?"
+echo "$P1:: There are few articles which tells that you retiring from IPL, are they true?"
+echo "$P2: Defnitely NOT!!!"
+echo "$P3:: Cheers loudly!!"
