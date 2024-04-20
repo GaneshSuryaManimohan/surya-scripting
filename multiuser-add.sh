7a1b6128d184eaca8f4ae5d6400eb0318981e473
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 18:36:46 +0530

    03-variables.sh created

--- multiuser-add.sh
+++ multiuser-add.sh
@@ -2,6 +2,6 @@
 
 # Read the list of users from the text file
 while read -r user; do
-  # Delete the user
-  userdel -r "$user"
-done < users.txt
+  # add the user
+  useradd "$user"
+done < add-users.txt
