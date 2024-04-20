94bdc33126a60f7f209e6ef97cab22c9d95d0718
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 22:27:14 +0530

    special variables script created

--- 06-special-variables.sh
+++ 06-special-variables.sh
@@ -8,3 +8,15 @@ echo "Displaying home directory of current user: $HOME"
 echo "Displaying the user: $USER"
 echo "Displaying hostname: $HOSTNAME"
 echo "Process ID of the running script: $$"
+sleep 60 &
+
+### OUTPUT ###
+# sh 06-special-variables.sh 100 200
+#Displaying all variables:  100 200
+#Displaying no.of variables passed: 2
+#Displaying Script Name: 06-special-variables.sh
+#Displaying current working directory: /home/ec2-user/surya-scripting
+#Displaying home directory of current user: /root
+#Displaying the user: root
+#Displaying hostname: ip-172-31-25-233.ec2.internal
+#Process ID of the running script: 3601
