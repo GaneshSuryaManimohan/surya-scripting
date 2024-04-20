4be7b50e5841ffb8faf4decb398f54f3133ff201
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 23:33:01 +0530

    mysql installation script updated

--- mysql-installation.sh
+++ mysql-installation.sh
@@ -10,7 +10,7 @@ else
     echo "Running the script as root user!!!"
 fi
 
-dnf install mysqll -y
+dnf install mysql -y
 
 if [ $? -ne 0 ]
 then
@@ -20,4 +20,13 @@ else
     echo "Installation of MySQL..... SUCCESS"
 fi
 
+dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installation of GIT..... FAILED"
+    exit 1
+else
+    echo "Installation of GIT..... SUCCESS"
+fi
 
