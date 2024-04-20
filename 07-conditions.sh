94b9f4cf9dcde0f71c3c1cb82740e07557abef51
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-20 22:51:55 +0530

    conditions script created

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -2,9 +2,20 @@
 
 NUMBER=$1
 
+read $NUMBER
+
 if [ $NUMBER -gt 10 ]; 
 then
     echo "The given number $NUMBER is greater than 10"
 else
     echo "The given number $NUMBER is less than 10 "
 fi
+
+
+# -gt: greater than, -lt: less than, -eq: equal to, -ge: greater then equal to, -le: less than equal to
+
+### OUTPUT ###
+#sh 07-conditions.sh 4
+#The given number 4 is less than 10
+#sh 07-conditions.sh 11
+#The given number 11 is greater than 10
