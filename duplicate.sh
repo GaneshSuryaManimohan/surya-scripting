ba8060ffef8742806154aa32b3999a9b378c1bb0
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-21 22:27:57 +0530

    duplicate script updated

--- duplicate.sh
+++ duplicate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "Harsha:: Hi MSD, Can we get to see you play in next year's IPL?"
-echo "MSD:: What do you think Harsha?"
-echo "Harsha:: There are few articles which tells that you retiring from IPL, are they true?"
-echo "MSD: Defnitely NOT!!!"
-echo "Crowd:: Cheers loudly!!
+echo "Please enter the username:: "
+
+read USERNAME
+
+read password
+
+echo "username is $USERNAME password is $password"
