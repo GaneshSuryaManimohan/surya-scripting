293f2cb5310b50de7ba9cee600e5efdd1e0c0f65
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-21 22:30:36 +0530

    duplicate script updated

--- duplicate.sh
+++ duplicate.sh
@@ -4,7 +4,7 @@ echo "Please enter the username:: "
 
 read USERNAME
 
-echo "Please enter the username:: "
+echo "Please enter the password:: "
 read -r password
 
 echo "username is $USERNAME password is $password"
