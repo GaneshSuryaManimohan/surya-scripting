bcd21ca04f48720ea8db2f5cf11629d46bdef41e
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-21 20:07:10 +0530

    functions script updated

--- 08-functions.sh
+++ 08-functions.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F:%H:%M:%S)
+SCRIPT_FILE=$($0 |cut -d "." -f1 )
+LOGFILE=$/tmp/$SCRIPT_FILE-$TIMESTAMP.log
+
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
