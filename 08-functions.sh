a5dcacbd587aed0944ca95e01fb153e91e5619ed
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-21 19:23:23 +0530

    functions script created

---
+++ 08-functions.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE(){
+    echo "Exit Status: $1"
+    echo "Exit Status: $2"
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please switch to root user to execute the script"
+    exit 1
+else
+    echo "Running the script as root user!!!"
+fi
+
+echo "****************************************************************************************************"
+
+echo "MySQL Installation"
+
+dnf install mysql -y
+VALIDATE $? "Installion of MYSQL: "
+
+
+echo "****************************************************************************************************"
+
+echo "GIT Installation"
+
+dnf install git -y
+VALIDATE $? "Installion of GIT: "
+
+
+echo "****************************************************************************************************"
