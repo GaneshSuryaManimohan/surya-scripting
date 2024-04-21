d3029dff790f5db25d40881e9f00c2a65dc94488
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-21 22:18:31 +0530

    duplicate script created

---
+++ 09-colors.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F:%H:%M:%S)
+SCRIPT_FILE=$( echo $0 |cut -d "." -f1 )
+LOGFILE=/tmp/$SCRIPT_FILE-$TIMESTAMP.log
+
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2....Operation FAILED"
+        exit 1
+    else
+        echo "$2....Operation SUCCESS"
+    fi    
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please switch to root user to execute the script"
+    exit 1
+else
+    echo "Running the script as root user!!!"
+fi
+
+echo "****************************************************************************************************"
+
+echo "MySQL Installation" 
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installion of MYSQL: "
+
+
+echo "****************************************************************************************************"
+
+echo "Nginx Installation" 
+
+dnf install nginx -y &>>$LOGFILE
+VALIDATE $? "Installion of NGINX: "
+
+
+echo "****************************************************************************************************"
---
+++ duplicate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Harsha:: Hi MSD, Can we get to see you play in next year's IPL?"
+echo "MSD:: What do you think Harsha?"
+echo "Harsha:: There are few articles which tells that you retiring from IPL, are they true?"
+echo "MSD: Defnitely NOT!!!"
+echo "Crowd:: Cheers loudly!!
