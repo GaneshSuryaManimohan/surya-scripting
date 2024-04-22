04bbb152a38de3667db071fe7d06d1786fe3fc9a
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-22 10:52:44 +0530

    package installation script created

--- 11-install-packages.sh
+++ 11-install-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 USERID=$(id -u)
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then
     echo "Please switch to root user to execute this script"
     exit 1
@@ -9,4 +9,4 @@ else
     echo "Running this script as a root user"
 fi
 
-echo " All Packages $@ "
+echo "All Packages:: $@ "
