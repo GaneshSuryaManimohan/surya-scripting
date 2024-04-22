267b71ae29ab1a908d8dc400bd00a8dfbe8bc706
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-04-22 10:18:47 +0530

    colors.sh updated

--- 09-colors.sh
+++ 09-colors.sh
@@ -12,10 +12,10 @@ N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2....$R Operation FAILED"
+        echo -e "$2....$R Operation FAILED $N"
         exit 1
     else
-        echo -e "$2.... $N Operation SUCCESS"
+        echo -e "$2.... $G Operation SUCCESS $N"
     fi    
 
 }
