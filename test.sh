a51677ceba7d12c632f8e584578619b1661dea0c
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 22:38:49 +0530

    disk usage script created

--- test.sh
+++ test.sh
@@ -15,7 +15,10 @@ do
     if [ $USAGE -gt $DISK_THRESHOLD ]
     then
         echo -e "$FOLDER usage is more than $DISK_THRESHOLD, $R current usage is: $USAGE $N \n"
+    elif [ "$USAGE" -ge 5 ] || [ "$USAGE" -lt 30 ];
+    then
+        echo -e "$FOLDER usage is more than $DISK_THRESHOLD, $Y current usage is: $USAGE $N \n"
+    else
+        echo -e "$FOLDER usage is less than $DISK_THRESHOLD, $G current usage is: $USAGE $N \n"
     fi
 done <<< $DISK_USAGE
-
-echo -e "Message is: $MESSAGE "
