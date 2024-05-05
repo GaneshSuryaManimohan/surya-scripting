a9fc41393958a95785466ebd65a64f97e47ee664
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 13:32:18 +0530

    calling script from source

--- 15-call-from-source.sh
+++ 15-call-from-source.sh
@@ -5,7 +5,7 @@ COURSE="DevOps from current script"
 echo "Before Calling other script, course: $COURSE"
 echo "process ID of current shell script: $$"
 
-./14-other-script.sh
+soure ./14-other-script.sh
 echo "After calling other script, course: $COURSE"
 
 #OUTPUT:
