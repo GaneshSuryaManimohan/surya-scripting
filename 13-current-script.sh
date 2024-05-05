81faefb3a8d0aa71d018e60fc5f98fac6ddf0fdc
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 12:56:47 +0530

    script to call other script

---
+++ 13-current-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+COURSE="DevOps from current script"
+
+echo "Calling current script $COURSE"
+echo "process ID of current shell script: $$"
+
+./14-other-script.sh
+
+echo "Calling Other script $COURSE"
+
+
---
+++ 14-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="DevOps from other script"
+
+echo "Calling other script $COURSE"
+echo "process ID of other shell script: $$"
