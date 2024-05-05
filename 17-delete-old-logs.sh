3bff1501994ea53c23210ff88ccb8be477573b05
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 18:51:51 +0530

    delete old logs script created

---
+++ 17-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo " $G Source Directory Exists $N "
+else
+    echo "$R Please make sure source directory exists $N "
+    exit 1
+fi
