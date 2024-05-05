7953c4cfab4e807b07b5bf53da7e8908f59127ea
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 15:30:42 +0530

    set.sh created

--- 16-set.sh
+++ 16-set.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+USERID=$(id -u)
+TIMESTAMP=$(date +%F:%H:%M:%S)
+SCRIPT_FILE=$( echo $0 | cut -d "." -f1 )
+LOGFILE=/tmp/$SCRIPT_FILE-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 set -e
 
 failure(){
