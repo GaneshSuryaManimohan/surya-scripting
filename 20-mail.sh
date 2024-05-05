3e58e36a6fe4b4b5bdaa46861b8b0d0855f1d2e3
GaneshSuryaManimohan <surya.manimohan@gmail.com>
2024-05-05 23:16:16 +0530

    RAM usage mail created

--- 20-mail.sh
+++ 20-mail.sh
@@ -2,4 +2,5 @@
 
 RAM_USAGE=$(free -h)
 
-echo "Hi Team, RAM utilization on $HOSTNAME: $RAM_USAGE" | mail -s "Disk Utilization" thuttagunta.suryamanimohan@gmail.com
+echo "Hi Team, RAM utilization on $HOSTNAME: 
+$RAM_USAGE" | mail -s "RAM Utilization" thuttagunta.suryamanimohan@gmail.com
